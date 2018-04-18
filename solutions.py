cd56142363ef1554d712052e2ccb4131a9350874
Giancarlo Giuffra <giancarlo.giuffra.moncayo@gmail.com>
2018-04-18 20:47:27 +0200

    completed first part of second execise and relative pandas part

--- solutions.py
+++ solutions.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 def EuropeanOptionMC(forward, strike, discount_factor, time_to_maturity, volatility, number_of_samples, flag):
     standar_normal = np.random.randn(number_of_samples, 1)
@@ -10,3 +11,6 @@ def EuropeanOptionMC(forward, strike, discount_factor, time_to_maturity, volatil
         simulated_option_price = discount_factor*(strike - asset_at_maturity).clip(0, np.inf)
 
     return np.mean(simulated_option_price)
+
+def ReadSwapRates(file, sheet, fromRow, columnRange):
+    return pd.read_excel(file, sheet_name = sheet, skiprows = fromRow, usecols = columnRange)
--- tests.py
+++ tests.py
@@ -26,7 +26,7 @@ class EuropeanOptionMCTest(TestCase):
         expected_price = solutions.EuropeanOptionMC(forward, option.strike, discount_factor, option.time_to_maturity, option.volatility, number_of_samples, 1)
         np.random.seed(218423)
         actual_price = self.student_function(forward, option.strike, discount_factor, option.time_to_maturity, option.volatility, number_of_samples, 1)
-        self.assertEqual(expected_price, actual_price)
+        self.assertAlmostEqual(expected_price, actual_price)
         
     def test_put(self):
         option = EuropeanOption()
@@ -37,5 +37,37 @@ class EuropeanOptionMCTest(TestCase):
         expected_price = solutions.EuropeanOptionMC(forward, option.strike, discount_factor, option.time_to_maturity, option.volatility, number_of_samples, -1)
         np.random.seed(218423)
         actual_price = self.student_function(forward, option.strike, discount_factor, option.time_to_maturity, option.volatility, number_of_samples, -1)
-        self.assertEqual(expected_price, actual_price)
+        self.assertAlmostEqual(expected_price, actual_price)
         
+
+class ReadSwapRatesTest(TestCase):
+
+    file = 'MktData_CurveBootstrap.xls'
+    sheet = 'Data'
+    fromRow = 37
+    columnRange = 'D:F'
+    
+    def __init__(self, testname, student_function):
+        super(ReadSwapRatesTest, self).__init__(testname)
+        self.student_function = student_function
+        self.expected_data_frame = solutions.ReadSwapRates(self.file, self.sheet, self.fromRow, self.columnRange)
+
+    def test_header(self):
+        actual_data_frame = self.student_function(self.file, self.sheet, self.fromRow, self.columnRange)
+        self.assertEqual(self.expected_data_frame.columns.tolist(), actual_data_frame.columns.tolist())
+
+    def test_number_of_rows(self):
+        actual_data_frame = self.student_function(self.file, self.sheet, self.fromRow, self.columnRange)
+        self.assertEqual(self.expected_data_frame.shape[0], actual_data_frame.shape[0])
+
+    def test_dates(self):
+        actual_data_frame = self.student_function(self.file, self.sheet, self.fromRow, self.columnRange)
+        self.assertEqual(self.expected_data_frame.ix[:,0].tolist(), actual_data_frame.ix[:,0].tolist())
+
+    def test_bids(self):
+        actual_data_frame = self.student_function(self.file, self.sheet, self.fromRow, self.columnRange)
+        self.assertEqual(self.expected_data_frame.ix[:,1].tolist(), actual_data_frame.ix[:,1].tolist())
+
+    def test_asks(self):
+        actual_data_frame = self.student_function(self.file, self.sheet, self.fromRow, self.columnRange)
+        self.assertEqual(self.expected_data_frame.ix[:,2].tolist(), actual_data_frame.ix[:,2].tolist())
